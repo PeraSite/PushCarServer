ac8a4024cfaafe2c1f018b0a79bfcd414dab7dcb
PeraSite <pretocki3@gmail.com>
2023-04-14 10:53:08 +0900

    chore: User 모델 추가

---
+++ PushCar.Common/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace PushCar.Common.Models {
+	public struct User {
+		public string Id { get; }
+		public string EncryptedPassword { get; }
+
+		public User(string id, string encryptedPassword) {
+			Id = id;
+			EncryptedPassword = encryptedPassword;
+		}
+	}
+}
